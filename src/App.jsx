11e7f7df2822f4a8c1b5c7b75f3b30936c06c995
HungHSU <hung.thh4969@sinhvien.hoasen.edu.vn>
2022-03-28 10:21:53 +0700

    learing Todo App with reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,115 @@ import Content from './Content';
 import { useReducer } from 'react';
 import './App.css';
 
-// Init states
-const initStates = 0;
+//1. Init states
 
-// Actions
-const UP_STATES = 'up'
-const DOWN_STATES = 'down'
+const initStates = {
+  job: '',
+  jobs: []
+}
 
-// Reducer
-const reducer = (state, action) => {
-  switch (action) {
-    case UP_STATES:
-      return state + 1
-    case DOWN_STATES:
-      return state - 1
-    default: new Error('invalid error')
+//2. Actions
+
+const INPUT_STATES = 'input'
+const CREATE_STATES = 'create'
+const DELETE_STATES = 'delete'
+
+const setJob = payload => {
+  return {
+    type: INPUT_STATES,
+    payload
+  }
+}
+
+const createJob = payload => {
+  return {
+    type: CREATE_STATES,
+    payload
+  }
+}
+
+const deleteJob = payload => {
+  return {
+    type: DELETE_STATES,
+    payload
+  }
+}
+
+//3. Reducer
+
+const reducer = (states, action) => {
+
+  let newState
+
+  switch (action.type) {
+    case INPUT_STATES:
+      newState = {
+        ...states,
+        job: action.payload
+      }
+      break
+    case CREATE_STATES:
+      newState = {
+        ...states,
+        jobs: [...states.jobs, action.payload]
+      }
+      break
+    case DELETE_STATES:
+      const newJobs = [...states.jobs]
+
+      newJobs.splice(action.payload, 1)
+
+      newState = {
+        ...states,
+        jobs: newJobs
+      }
+      break
+    default:
+      throw new Error('Invalid action.')
   }
 
+  return newState
 }
 
 function App() {
 
-  const [cost, dispatch] = useReducer(reducer, initStates)
+  const [states, dispatch] = useReducer(reducer, initStates)
 
+  const { job, jobs } = states
 
+  // const handleDelete = (index) => {
+  //   dispatch(deleteJob(index))
+  // }
+
+  const handleSubmit = () => {
+    dispatch(createJob(job))
+    dispatch(setJob(''))
+  }
 
   return (
 
     <div style={{ padding: "10px 32px" }}>
 
-      <h1>{cost}</h1>
-      <br />
+      <input
+        value={job}
+        type="text"
+        onChange={e => { dispatch(setJob(e.target.value)) }}
+      />
       <button
-        onClick={() => dispatch(UP_STATES)}
-      >Up</button>
-      <button
-        onClick={() => dispatch(DOWN_STATES)}
-      >Down</button>
+        onClick={handleSubmit}
+      >Add</button>
+      <ul>
+        {jobs.map((e, index) => (
+          <li key={index}>
+            {e}
+            <span
+              onClick={() => { dispatch(deleteJob(index)) }}
+            >&times;</span>
+          </li>
+        ))}
+      </ul>
+
+
 
     </div>
   )
